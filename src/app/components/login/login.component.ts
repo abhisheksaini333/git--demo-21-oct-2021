b3f026c0fcc782f56aa2cd4bf7663085778e13dd
abhisheksaini333 <abhishek_saini@live.com>
2021-10-21 19:38:31 +0530

    login created

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,14 @@
+
+
+@CompositionEvent(
+    {
+        selector: 'app-login',
+        template: `<h1>Login</h1>`,
+        styles: [ `h1{ color: red }` ]
+    }
+)
+export default class LoginComponent {
+    constructor() {
+
+    }
+}
